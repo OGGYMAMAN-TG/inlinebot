15a086b390a926d0ad65bbccc6426938aaf489d0
irash1234567 <85207400+irash1234567@users.noreply.github.com>
2021-09-24 16:57:08 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/mallumovie11')
+            InlineKeyboardButton('More Bots', url='https://t.me/ULTRONBOTS')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
